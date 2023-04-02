38c2971545316a1731ed992fd7ac83343c7fa9ac
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-04-02 12:41:37 +0200

    Add else if in script.js

--- js/js/script.js
+++ js/js/script.js
@@ -1,5 +1,18 @@
-let computerMove = `kamień`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-let playerMove = `papier`;
+console.log('Gracz wpisał: ' + playerInput);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+} else if (playerInput == '2'){
+    playerMove = 'papier';
+}
+
+} else if (playerInput == '3'){
+    playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
