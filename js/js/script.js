1a6b4c5c84f402ed4957dd1f32ccb183f03071e1
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-04-02 13:14:12 +0200

    Add else if 3 in script.js

--- js/js/script.js
+++ js/js/script.js
@@ -4,17 +4,17 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
+if (randomNumber == 1) {
+    computerMove = 'kamień';
 }
 
-else if(randomNumber == 2){
+else if (randomNumber == 2) {
     computerMove = 'papier';
-  }
+}
 
-else if(randomNumber == 3){
+else if (randomNumber == 3) {
     computerMove = 'nożyce';
-  }
+}
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -25,15 +25,38 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
+if (playerInput == '1') {
+    playerMove = 'kamień';
 }
 
-} else if (playerInput == '2'){
+} else if (playerInput == '2') {
     playerMove = 'papier';
 }
 
-} else if (playerInput == '3'){
+} else if (playerInput == '3') {
     playerMove = 'nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Ty pregrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Ty pregrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Ty pregrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'nieznany ruch') {
+    printMessage('Ty pregrywasz!');
+}
